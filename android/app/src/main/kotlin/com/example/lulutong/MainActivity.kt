6f96d3e84a30b7fa5a7faa82deaaf5c1d8d7a143
Ricky Lau <lauyin8183@gmail.com>
2024-12-10 14:43:59 +1000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lulutong/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lulutong
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text("My On9 App"),
+      backgroundColor: Colors.brown[700],
+      centerTitle: true,
+    ),
+    body: const Home(),
+  )));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Hello Ricky!!!!!",
+    );
+  }
+}
